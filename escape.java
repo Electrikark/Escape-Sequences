ca4b2d0792bb4c8613ab7dd0fb3bef13ca1899a4
Naman Raichura <naman.raichura@gmail.com>
2026-08-11 13:15:38 -0700

    Create escape variables file code - Day 2

---
+++ escape.java
@@ -0,0 +1,13 @@
+public class escape{ // has to mathc name of file
+    public static void main(String[] args){
+
+        // Escape sequences: \\ , \n, \r, \", \b
+
+        System.out.println("C:\\Users\\Docs \r File Extension \nNote: \"Staff Don't Open\" AA\b " ); // produces a single slash
+        
+        // \n creates new line
+
+    }
+
+
+}
