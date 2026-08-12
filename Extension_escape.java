61862742682de87609bbf8636ad77bbaeacceca7
Naman Raichura <naman.raichura@gmail.com>
2026-08-12 12:17:54 -0700

    Doing the extension assignment

---
+++ Extension_escape.java
@@ -0,0 +1,44 @@
+public class Extension_escape {
+
+    public static void main(String[] args) {
+
+        // Escape Sequences
+        System.out.println("\"Quotes\"");
+        System.out.println("Slashes \\//");
+        System.out.println("How '\"confounding' \"\\\" it is!");
+
+        System.out.println();
+
+        // Archie
+        System.out.println("name\tage\theight");
+        System.out.println("Archie\t17\t5'9\"");
+        System.out.println("Betty\t17\t5'6\"");
+        System.out.println("Jughead\t16\t6'");
+
+        System.out.println();
+
+        // Downward Spiral
+        System.out.println("\ta\tb\tc");
+        System.out.println("\\\\");
+        System.out.println("'");
+        System.out.println("\"\"\"");
+        System.out.println("C:\\in\\the downward spiral");
+
+        System.out.println();
+
+        // DoubleSlash
+        System.out.println("Dear \"DoubleSlash\" magazine,");
+        System.out.println();
+        System.out.println("\tYour publication confuses me. Is it");
+        System.out.println("a \\\\ slash or a //// slash?");
+        System.out.println("\nSincerely,");
+        System.out.println("Susan \"Suzy\" Smith");
+
+        System.out.println();
+
+        // Sally
+        System.out.println("\"Several slashes are sometimes seen,\"");
+        System.out.println("said Sally. \"I've said so.\" See?");
+        System.out.println("\\ / \\\\ // \\\\\\ ///");
+    }
+}
--- escape.java
+++ escape.java
@@ -1,11 +1,22 @@
 public class escape{ // has to mathc name of file
     public static void main(String[] args){
 
-        // Escape sequences: \\ , \n, \r, \", \b
+        // Escape sequences: \\ , \n, \r, \", \b, \t
 
-        System.out.println("C:\\Users\\Docs \r File Extension \nNote: \"Staff Don't Open\" AA\b " ); // produces a single slash
+        System.out.println(" C:\\Users\\Docs \r File Extension  \n\tNote: \"Staff Don't Open\" AA\b\b\b YOU " ); // produces a single slash
         
         // \n creates new line
+        System.out.println("name\tage\theight");
+        System.out.println("\"\"\"");
+
+        System.out.println("\\Hello");
+        System.out.println("Slashes \\//");
+        System.out.println("Jughead\t16\t6'");
+        System.out.println("\\\\\\");
+
+
+
+
 
     }
 
